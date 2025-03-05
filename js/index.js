6ceba6c8052ac3cf34811e22bc3f983fc275b9d7
fharhanamrin <fharhanamrin028@gmail.com>
2025-03-05 19:58:13 +0700

    remove comment code

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-let menuIconMobile = document.querySelector(".menu-icon-mobile");
-let menuMobile = document.querySelector(".menu-mobile");
+const menuIconMobile = document.querySelector('.menu-icon-mobile');
+const menuMobile = document.querySelector('.menu-mobile');
 
-menuIconMobile.addEventListener("click", () => {
-    menuMobile.style.display = "block";
-    console.log("clicked to show menu mobile");
+menuIconMobile.addEventListener('click', () => {
+    menuMobile.style.display = 'block';
+    console.log('clicked to show menu mobile');
 });
 
-menuMobile.addEventListener("click", () => {
-    menuMobile.style.display = "none";
-    console.log("clicked to hidden menu mobile");
+menuMobile.addEventListener('click', () => {
+    menuMobile.style.display = 'none'; 
+    console.log('clicked to hide menu mobile');
 });
