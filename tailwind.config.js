aa13419daac5b18610a01d62d0cbdfbb6982e53e
fharhanamrin <fharhanamrin028@gmail.com>
2025-04-04 21:06:49 +0700

    Add Tailwind CSS configuration and build script to package.json; create tailwind.config.js for content and theme settings

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}",
+        "./src/about/**/*.{html,js}",
+        "./src/contact/**/*.{html,js}",
+        "./src/portfolio/**/*.{html,js}",
+        "./src/index.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
